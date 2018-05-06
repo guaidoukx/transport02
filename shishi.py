95f019f9173edfaec8702d7b074584f4ee4fb53f
XiangYali <172886134@qq.com>
2018-05-06 20:50:50 +0800

    yao gaidong yao la

--- shishi.py
+++ shishi.py
@@ -1,26 +1,15 @@
 import pandas as pd
-D = {-1:([4,'s'],[5])}
-print(D[-1][0])
-C = ['a']
-B = ['c','d']
-print(pd.concat([pd.Series(B)]))
-m = C+B
-print(m)
-C1 = pd.concat([pd.Series(C+B)])
-print(C1)
-# def a(t=[],p=[]):
-#     return t,p
-#
-# # D[1] = a([1,2], [3,4])
-# print(a([1,2], [3,4]))
-# print(D[-1][1])
-# for i in D[-1]:
-#     B.append(pd.Series(i))
-#
-# print(pd.concat(B))
-# B.append(['mdn'])
-# print(B)
-# print('=================')
-# L = [1,]
-# L.append('s')
-# print(L)
+
+class Stu(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def add(self, t):
+        L = [t]
+        self.L = L
+        
+Astu = Stu('A', 2)
+print(Astu.L)
+Astu.add(3)
+print(Astu.L)
--- transport201804_02.py
+++ transport201804_02.py
@@ -3,13 +3,15 @@ import pandas as pd
 import numpy as np
 from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.cross_validation import KFold
+from sklearn.cross_validation import KFold, train_test_split
 from keras.layers.core import Dense
 from keras.models import Sequential
-# from compiler import flatten
+import xgboost as xgb
 
-example_trn = pd.read_csv('IO/test_after_n.csv')
+example_trn = pd.read_csv('IO/train_after_n.csv')
 example_ten = pd.read_csv('IO/test_after_n.csv')
+example_tr = pd.read_csv('IO/train_after.csv')
+example_te = pd.read_csv('IO/test_after.csv')
 
 
 def MAD(target, predictions):
@@ -21,10 +23,12 @@ class ReadData(object):
     def __init__(self, data, col_Y):
         self.data = data
         self.col_Y = col_Y
-
+    
     def split_to_kfold(self, overall, split):
         pdY = self.data[self.col_Y]
         pdX = self.data.drop([self.col_Y], axis=1)
+        index = np.arange(0, len(pdY))
+        np.random.shuffle(index)
         Y = pdY.values
         X = pdX.values
         kf = KFold(overall, n_folds=split, shuffle=True)
@@ -35,13 +39,13 @@ class ReadData(object):
             te_X = X[te_index]
             te_Y = Y[te_index]
             L.append([tr_X, tr_Y, te_X, te_Y])
-        return L, kf
-
+        self.L, self.kf = L, kf
+    
     def Train(self, model_name, tr_X, tr_Y):
         if model_name == 'SVR':
             models = SVR(kernel='poly', degree=4, gamma=1.8)
             models.fit(tr_X, tr_Y)
-            return models
+            self.model = models
         elif model_name == 'NN':
             models = Sequential()
             models.add(Dense(input_dim=62, units=500, activation='relu'))
@@ -51,67 +55,109 @@ class ReadData(object):
             models.add(Dense(units=200, activation='relu'))
             models.add(Dense(units=100, activation='relu'))
             models.add(Dense(units=1))
-
             models.compile(loss='mse', optimizer='Adam')
-            models.fit(tr_X, tr_Y, batch_size=100, epochs=2)
-            return models
+            models.fit(tr_X, tr_Y, batch_size=100, epochs=70)
+            self.model = models
+        elif model_name == 'XGBOOST':
+            train_x, val_x = train_test_split(tr_X, test_size=0.2, random_state=20)
+            train_y, val_y = train_test_split(tr_Y, test_size=0.2, random_state=20)
+            xgb_train = xgb.DMatrix(train_x, train_y)
+            xgb_val = xgb.DMatrix(val_x, val_y)
+            self.parameters = {
+                'booster' : 'gbtree',
+                'objective' : 'reg:linear',
+                'eval_metric': 'rmse',
+                'gamma': 0.1,
+                'max_depth': 8,
+                'subsample':0.9,
+                'colsample_bytree': 0.9,
+                'eta': 0.06
+            }
+            self.num_rounds = 150
+            self.watchlist = [(xgb_train, 'train'), (xgb_val, 'val')]
+            self.model = xgb.train(self.parameters, xgb_train, self.num_rounds, self.watchlist, early_stopping_rounds=100)
         else:
-            print("Error Parameter! It must be 'SVR' or 'NN'. " )
-
-    def printResult(self, model_name, models,tr_X, tr_Y, te_X, te_Y):
+            print("Error Parameter! It must be 'SVR', 'NN' or 'XGBOOST'. " )
+    
+    def evaluation(self, Y, pred, way):
+        if way =='RMSE':
+            print("Root Mean Squared Error: %.3f" % np.sqrt(mean_squared_error(Y, pred)))
+        elif way == 'R2':
+            print('R2 score: %.5f' % r2_score(Y,pred))
+        elif way == 'MAD':
+            print("Mean Absolute Deviation: %.3f" % MAD(Y, pred) + "\r" )
+        else:
+            print("Error Parameter! It must be 'RMSE','R2' or 'MAD'.")
+    
+    def printResult(self, model_name,tr_X, tr_Y, te_X, te_Y):
         if model_name == 'NN':
-            result = models.evaluate(te_X, te_Y)
-            print('\nNeural Network Training Finish & result')
-            print('Test result loss:', result)
-        tr_pred = models.predict(tr_X)
-        te_pred = models.predict(te_X)
-
-        def evaluation(Y, pred, way):
-            if way =='RMSE':
-                print("Root Mean Squared Error: %.3f" % np.sqrt(mean_squared_error(Y, pred)))
-            elif way == 'R2':
-                print('R2 score: %.3f' % r2_score(Y,pred))
-            elif way == 'MAD':
-                print("Mean Absolute Deviation: %.3f" % MAD(Y, pred) + "\r" )
-            else:
-                print("Error Parameter! It must be 'RMSE','R2' or 'MAD'.")
-
+            result = self.model.evaluate(te_X, te_Y)
+            # print('\nNeural Network Training Finish & result')
+            # print('Test result loss:', result)
+        tr_pred = self.model.predict(tr_X)
+        te_pred = self.model.predict(te_X)
+        
         print('---' + model_name + ' Train---')
-        evaluation(tr_Y, tr_pred, 'RMSE')
-        evaluation(tr_Y, tr_pred, 'R2')
-        evaluation(tr_Y, tr_pred, 'MAD')
+        self.evaluation(tr_Y, tr_pred, 'RMSE')
+        self.evaluation(tr_Y, tr_pred, 'R2')
+        self.evaluation(tr_Y, tr_pred, 'MAD')
         print('---' + model_name + ' Test---')
-        evaluation(te_Y, te_pred, 'RMSE')
-        evaluation(te_Y, te_pred, 'R2')
-        evaluation(te_Y, te_pred, 'MAD')
-        return te_Y, te_pred
+        self.evaluation(te_Y, te_pred, 'RMSE')
+        self.evaluation(te_Y, te_pred, 'R2')
+        self.evaluation(te_Y, te_pred, 'MAD')
+        self.te_y = te_Y.tolist()
+        self.te_pred = te_pred.flatten().tolist()
+    
+    def train_test_print(self,  model_name):
+        self.D = {-1:([],[])}
+        for t, pred in enumerate(self.kf):
+            tr_X, tr_Y, te_X, te_Y = self.L[t][0], self.L[t][1], self.L[t][2], self.L[t][3]
+            print('\n----%d iteration of ' % (t + 1) + model_name + ' ----')
+            self.printResult(model_name, tr_X, tr_Y, te_X, te_Y)
+            self.D[t] = (self.D[t-1][0]+self.te_y, self.D[t-1][1]+self.te_pred)
+            # print(self.D[t])
+        print('\n--- '+ model_name+' final resutl---')
+        self.evaluation(self.D[4][0], self.D[4][1],'RMSE')
+        self.evaluation(self.D[4][0], self.D[4][1],'R2')
+        # self.evaluation(np.ndarray(self.D[4][0]), np.ndarray(self.D[4][1]),'MAD')
 
-    def train_test_print(self,  model_name, t): #, yt= [], yp=[]
-        # self.y_true = yt
-        # self.y_pred = yp
-        self.model = self.Train(model_name, tr_X, tr_Y)
-        print('\n----%d iteration of ' % (t + 1) + model_name + ' ----')
-        te_y, te_pred = self.printResult(model_name, self.model, tr_X, tr_Y, te_X, te_Y)
-        # self.y_true.append(te_y)
-        # self.y_pred.append(te_pred)
-        # return self.y_true, self.y_pred
-        return te_y.tolist(), te_pred.flatten().tolist()
+    def XG(self):
+        self.D = {-1: ([], [])}
+        for t, pred in enumerate(self.kf):
+            tr_X, tr_Y, te_X, te_Y = self.L[t][0], self.L[t][1], self.L[t][2], self.L[t][3]
+            self.te_pred = self.model.predict(xgb.DMatrix(te_X)).tolist()
+            # print(type(self.te_pred))
+            # print(type(te_Y))
+            # print(len(self.te_pred))
+            # print(len(te_Y))
+            # print(self.te_pred)
+            # print(te_Y)
+            # print(self.te_pred.shape)
+            # print(te_Y.shape)
+            print('\n-----%d iteration xgboost Training Finish & result-----' % (t + 1))
+            print("Root Mean Squared Error: %.10f" % np.sqrt(mean_squared_error(te_Y, self.te_pred)))
+            print('R2 score: %.10f' % r2_score(te_Y, self.te_pred))
+            self.D[t] = (self.D[t-1][0] + te_Y.tolist(), self.D[t-1][1] + self.te_pred)
+        print(len(self.D[4][0]), len(self.D[4][1]))
+        
+        print("\n-----====final result===-----")
+        # print("final test Root Mean Squared Error: %.4f" % self.evaluation(self.D[4][0], self.D[4][1],'RMSE'))
+        print('final test R2 score: %.4f' % self.evaluation(self.D[4][0], self.D[4][1],'R2'))
 
-trdata = ReadData(example_trn, 'A')
-trdata.overall = len(trdata.data)
-trdata.L,  trdata.kf = trdata.split_to_kfold(trdata.overall, 5)
-trdata.model = trdata.Train('NN', trdata.L[0][0],trdata.L[0][1])
-trdata.D = {-1:([],[])}
 
-for t, pred in enumerate(trdata.kf):
-    tr_X, tr_Y, te_X, te_Y = trdata.L[t][0],trdata.L[t][1],trdata.L[t][2],trdata.L[t][3]
-    print(len(trdata.train_test_print('NN', t)[0]))
-    print(trdata.train_test_print('NN', t)[0])
-    trdata.D[t][0] = {trdata.D[t-1][0] + trdata.train_test_print('NN', t)[0]} #, trdata.D[t-1][0], trdata.D[t-1][1]
-    trdata.D[t][1] = np.hstack((trdata.D[t-1][1], trdata.train_test_print('NN', t)[1])) #, trdata.D[t-1][0], trdata.D[t-1][1]
-    print(len(trdata.D[0][1]))
-print('1111111111111111')
-# print(trdata.D[4][1])
+# trdata = ReadData(example_trn, 'A')
+# trdata.overall = len(trdata.data)
+# trdata.split_to_kfold(trdata.overall, 5)
+# trdata.Train('NN', trdata.L[0][0],trdata.L[0][1])
+# trdata.train_test_print('NN')
+# print(trdata.model.predict(example_ten.drop(['A'],axis=1)))
+# trdata.Train('SVR', trdata.L[0][0],trdata.L[0][1])
+# trdata.train_test_print('SVR')
+# print(trdata.model.predict(example_ten.drop(['A'],axis=1)))
 
-# print(pd.concat(pd.DataFrame([trdata.D[4][1]])))
+xgdata = ReadData(example_tr, 'A')
+xgdata.overall = len(xgdata.data)
+xgdata.split_to_kfold(xgdata.overall, 5)
+xgdata.Train('XGBOOST', xgdata.L[0][0], xgdata.L[0][1])
+xgdata.XG()
 
